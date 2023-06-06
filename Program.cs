59d20595fa3fcfd60298c97c35b834f402323c4e
Dmitriy <Dmitriy.Kh.78@yandex.ru>
2023-06-07 01:02:15 +0300

    Z3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число N: ");
+var stringN = Console.ReadLine();
+int n;
+bool isN = int.TryParse( stringN, out n );
+
+if (isN)
+
+for (int i = 1; i <= n; i++)
+{
+  Console.Write(Math.Pow(i, 3) + "\t");
+}
+else
+{
+    Console.Write("Ошибка! Повторите ввод  ");
+}
+
+
+
+
