4670b3f14315c64a9802fd7c7fd23340626b3f39
Dmitriy <Dmitriy.Kh.78@yandex.ru>
2023-06-07 01:33:50 +0300

    Z2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,29 @@
-﻿//Программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+  Console.Write("Введите координату точки А по оси X=");
+  int x1= Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите координату точки А по оси Y=");
+  int y1 = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите координату точки А по оси Z=");
+  int z1 = Convert.ToInt32(Console.ReadLine());
+
+  Console.Write("Введите координату точки В по оси X=");
+  int x2 = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите координату точки В по оси Y=");
+  int y2 = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите координату точки В по оси Z=");
+  int z2 = Convert.ToInt32(Console.ReadLine());
+
+  
+
+  double d = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+  Console.WriteLine($"Растояние между точками А:({x1}, {y1}, {z1}) и В:({x2},{y2}, {z2}) равно: {Math.Round(d,2)}");
+
+
+
+
+
+/*
+//Программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 Console.Write("Введите число N: ");
 var stringN = Console.ReadLine();
 int n;
@@ -14,7 +39,7 @@ else
 {
     Console.Write("Ошибка! Повторите ввод  ");
 }
-
+*/
 
 
 
